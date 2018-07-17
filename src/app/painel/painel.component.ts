063ff271a44d4ccf72a452633b81acc29a07bf58
Juliano Buzanello <jbcisne@gmail.com>
2018-07-16 22:16:15 -0300

    Implementação da logica de verificação de resposta e controle do progresso

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Frase } from '../shared/frase.model';
+import { FRASES } from './frases-mock';
 
 @Component({
   selector: 'app-painel',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelComponent implements OnInit {
 
-  constructor() { }
+  public instrucao: string = "Traduza a frase:"
+  public frases: Frase[] = FRASES
+  public resposta: string
+  public rodada: number = 0
+  public rodadaFrase: Frase
+  public progresso: number = 0
+
+  constructor() { 
+    this.atualizaRodada(this.rodada)
+    console.log(this.rodadaFrase)
+  }
 
   ngOnInit() {
+  }
+
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value;
+  }
+
+  public verificaResposta(): void {
+    console.log("Verificar resposta: ", this.resposta)
+    if (this.rodadaFrase.frasePtBr == this.resposta.trim()) {
+      alert('resposta correta. Parabéns')
+      this.progresso = this.progresso + (100 / this.frases.length)
+      this.atualizaRodada(++this.rodada)
+    } else {
+      alert('resposta errada')
+    }
+    
+  }
+
+  private atualizaRodada(rodada: number):void {
+    this.rodadaFrase = this.frases[rodada];
   }
 
 }
